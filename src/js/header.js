d6122d4e68a5d36b6156d7061dbe4f8a6b1a09c5
Vidiic <jordividal1997@gmail.com>
2023-12-04 14:56:43 +0100

    some styles & about me section

--- src/js/header.js
+++ src/js/header.js
@@ -1,9 +1,15 @@
+const header = document.querySelector('header');
 
 export function headerStyle(){
 
     const scrollHandle = (e) => {
         const scrollPosition = window.scrollY;
-        console.log(scrollPosition);
+        if(scrollPosition > 0){
+            header.classList.add('scroll');
+            return;
+        }
+        
+        header.classList.remove('scroll');
     }
 
     window.addEventListener('scroll', scrollHandle);
